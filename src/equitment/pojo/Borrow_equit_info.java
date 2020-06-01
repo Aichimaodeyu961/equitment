07715c10d3b787fda075c8a665195f0fe857a4b8
梁化海 <2814688636@qq.com>
2020-06-01 09:06:04 +0800

    新增借阅从表

---
+++ src/equitment/pojo/Borrow_equit_info.java
@@ -0,0 +1,10 @@
+package equitment.pojo;
+
+public class Borrow_equit_info {
+    private int borrow_equit_info_id;
+
+    private int borrow_equit_id;
+    private int equit_id;
+    private Equit equit;
+    private int equit_num;
+}
